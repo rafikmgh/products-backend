3a9c44c23a264b0d6ca61a2100f10120fdaa7359
MEGHOUCHE Rafik <rafikmeghouche@gmail.com>
2022-03-09 20:46:41 +0100

    configure-express-server

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express')
+const server = express()
+var cors = require('cors')
 
+var bodyParser = require('body-parser')
+server.use(express.json())
+server.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+server.use(cors({ credentials: true, origin: true }))
+
+server.use(express.static(__dirname + '/public'))
+
+// importing from the database
+const connectDB = require('./config/db')
+// connecting to the database
+connectDB()
+
+//running the express server into 5000 port
+var PORT = 5000 || process.env.PORT
+
+//using the product api
+var productRoute = require('./routes/Product')
+server.use('/api/products', productRoute)
+//using the user api
+var userRoute = require('./routes/User')
+server.use('/api/users', userRoute)
+
+server.listen(PORT, () => {
+  console.log(`SERVER RUNNING IN PORT ${PORT}`)
+})
